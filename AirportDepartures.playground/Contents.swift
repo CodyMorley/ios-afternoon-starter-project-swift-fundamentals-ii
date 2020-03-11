4e4e397757d51fbd86e1ce9d675967f7f7533ee6
Cody Morley <codymorleyinc@gmail.com>
2020-03-11 15:47:38 -0700

    changes to airportdepartures, new text file with completed instructions to reduce clutter

--- AirportDepartures.playground/Contents.swift
+++ AirportDepartures.playground/Contents.swift
@@ -1,63 +1,93 @@
 import UIKit
+import PlaygroundSupport
+PlaygroundPage.current.needsIndefiniteExecution = true
+// 1
+
+enum FlightStatus: String {
+    case enRoute = "En Route"
+    case scheduled = "Scheduled"
+    case canceled = "Canceled"
+    case delayed = "Delayed"
+    case landed = "Landed"
+}
+
+enum ArrivalOrDeparture {
+    case arrival
+    case departure
+}
+
+enum AirportName: String {
+    case clt = "Charlotte"
+    case gyd = "Baku"
+    case puj = "Punta Cana"
+    case nrt = "Tokyo"
+    case mvy = "Martha's Vineyard"
+    case slc = "Salt Lake City"
+    case cur = "Curacao"
+}
+
+struct Airport {
+    var name: AirportName
+}
+
+struct Flight {
+    var name: String
+    var airport: Airport
+    var arrivalOrDeparture: ArrivalOrDeparture
+    var terminal: String?
+    var time: Date?
+    var status: FlightStatus
+}
+
+class DepartureBoard {
+    var listOfDepartures: [Flight]
+    
+    init(listOfDepartures: [Flight]) {
+        self.listOfDepartures = listOfDepartures
+    }
+}
+//2
+let charlotte = Airport(name: .clt)
+let baku = Airport(name: .gyd)
+let putacana = Airport(name: .puj)
 
 
-//: ## 1. Create custom types to represent an Airport Departures display
-//: ![Airport Departures](matthew-smith-5934-unsplash.jpg)
-//: Look at data from [Departures at JFK Airport in NYC](https://www.airport-jfk.com/departures.php) for reference.
-//:
-//: a. Use an `enum` type for the FlightStatus (En Route, Scheduled, Canceled, Delayed, etc.)
-//:
-//: b. Use a struct to represent an `Airport` (Destination or Arrival)
-//:
-//: c. Use a struct to represent a `Flight`.
-//:
-//: d. Use a `Date?` for the departure time since it may be canceled.
-//:
-//: e. Use a `String?` for the Terminal, since it may not be set yet (i.e.: waiting to arrive on time)
-//:
-//: f. Use a class to represent a `DepartureBoard` with a list of departure flights, and the current airport
+var flight1 = Flight(name: "AY4165", airport: charlotte, arrivalOrDeparture: .departure, terminal: "8", time: Date(), status: .landed)
+var flight2 = Flight(name: "AA2809", airport: charlotte, arrivalOrDeparture: .departure, terminal: "8", time: Date(), status: .landed)
+var flight3 = Flight(name: "7l778", airport: baku, arrivalOrDeparture: .departure, terminal: nil, time: nil, status: .canceled)
+
+var myDepartureBoard = DepartureBoard(listOfDepartures: [flight1, flight2])
+
+myDepartureBoard.listOfDepartures.append(flight3)
+
+//3
+
+func printDepartures(board: DepartureBoard) {
+    
+    
+    for departure in board.listOfDepartures {
+        var unwrappedTerminal = ""
+        var unwrappedTime = ""
+        
+        if let terminal = departure.terminal {
+            unwrappedTerminal = terminal
+        } else {
+            unwrappedTerminal = "N/A"
+        }
+        
+        if let time = departure.time {
+            unwrappedTime = "\(time)"
+        } else {
+            unwrappedTime = "N/A"
+        }
+        
+        print("\(departure.name) \(departure.airport.name.rawValue) \(unwrappedTerminal) \(unwrappedTime) \(departure.status.rawValue)")
+    }
+}
+
+printDepartures(board: myDepartureBoard)
 
 
-
-//: ## 2. Create 3 flights and add them to a departure board
-//: a. For the departure time, use `Date()` for the current time
-//:
-//: b. Use the Array `append()` method to add `Flight`'s
-//:
-//: c. Make one of the flights `.canceled` with a `nil` departure time
-//:
-//: d. Make one of the flights have a `nil` terminal because it has not been decided yet.
-//:
-//: e. Stretch: Look at the API for [`DateComponents`](https://developer.apple.com/documentation/foundation/datecomponents?language=objc) for creating a specific time
-
-
-
-//: ## 3. Create a free-standing function that can print the flight information from the `DepartureBoard`
-//: a. Use the function signature: `printDepartures(departureBoard:)`
-//:
-//: b. Use a `for in` loop to iterate over each departure
-//:
-//: c. Make your `FlightStatus` enum conform to `String` so you can print the `rawValue` String values from the `enum`. See the [enum documentation](https://docs.swift.org/swift-book/LanguageGuide/Enumerations.html).
-//:
-//: d. Print out the current DepartureBoard you created using the function
-
-
-
-
-//: ## 4. Make a second function to print print an empty string if the `departureTime` is nil
-//: a. Createa new `printDepartures2(departureBoard:)` or modify the previous function
-//:
-//: b. Use optional binding to unwrap any optional values, use string interpolation to turn a non-optional date into a String
-//:
-//: c. Call the new or udpated function. It should not print `Optional(2019-05-30 17:09:20 +0000)` for departureTime or for the Terminal.
-//:
-//: d. Stretch: Format the time string so it displays only the time using a [`DateFormatter`](https://developer.apple.com/documentation/foundation/dateformatter) look at the `dateStyle` (none), `timeStyle` (short) and the `string(from:)` method
-//:
-//: e. Your output should look like:
-//:
-//:     Destination: Los Angeles Airline: Delta Air Lines Flight: KL 6966 Departure Time:  Terminal: 4 Status: Canceled
-//:     Destination: Rochester Airline: Jet Blue Airways Flight: B6 586 Departure Time: 1:26 PM Terminal:  Status: Scheduled
-//:     Destination: Boston Airline: KLM Flight: KL 6966 Departure Time: 1:26 PM Terminal: 4 Status: Scheduled
 
 
 
